35dfc8b3b65deff03f1f5412bf0f40761805671e
roland.pakai <roland.pakai@lightware.com>
2024-10-28 09:34:05 +0100

    React with AbortController

--- src/App.js
+++ src/App.js
@@ -1,23 +1,46 @@
-import logo from './logo.svg';
+import React, { useEffect, useState } from 'react';
+import { Link, useLocation } from 'react-router-dom';
 import './App.css';
 
 function App() {
+  const [user, setUser] = useState({});
+  const id = useLocation().pathname.split('/')[2];
+  const [error, setError] = useState(null);
+
+  useEffect(() => {
+    let didCancel = false;
+    const controller = new AbortController();
+    const signal = controller.signal;
+
+    const fetchData = async () => {
+      try {
+        const response = await fetch(`https://jsonplaceholder.typicode.com/users/${id}`, {signal})
+        const data = await response.json();
+        if (!didCancel) setUser(data);
+      } catch (e) {
+        if (!didCancel) setError(e.message);
+      }
+    }
+
+    if (id) fetchData();
+
+    return () => {
+      didCancel = true;
+      controller.abort();
+    };
+  }, [id]);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div>
+        <p>Name: {user.name}</p>
+        <p>Username: {user.username}</p>
+        <p>Email: {user.email}</p>
+        <p><Link to="/users/1">Fetch User 1</Link></p>
+        <p><Link to="/users/2">Fetch User 2</Link></p>
+        <p><Link to="/users/3">Fetch User 3</Link></p>
+        <p>Error: {error}</p>
+      </div>
     </div>
   );
 }
